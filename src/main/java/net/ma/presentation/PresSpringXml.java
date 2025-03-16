f7319632cc71ad082b3b3ab78277a5c5aae69084
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-16 03:46:42 +0000

    l'injection des dépendances   En utilisant le Framework Spring       Version XML

--- src/main/java/net/ma/presentation/PresSpringXml.java
+++ src/main/java/net/ma/presentation/PresSpringXml.java
@@ -1,4 +1,13 @@
 package net.ma.presentation;
 
+import net.ma.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class PresSpringXml {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier = (IMetier) context.getBean("m");
+        System.out.println(metier.calcul());
+    }
 }
