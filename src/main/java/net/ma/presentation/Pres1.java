feb07e96674a2b0baf766826830e754e193de36e
hamza omaddi <hamza.omaddi@gmail.com>
2025-03-16 03:44:37 +0000

    l'injection des dépendances   Par instanciation statique

--- src/main/java/net/ma/presentation/Pres1.java
+++ src/main/java/net/ma/presentation/Pres1.java
@@ -1,4 +1,14 @@
 package net.ma.presentation;
 
+import net.ma.ext.DaoImplV2;
+import net.ma.metier.MetierImpl;
+
 public class Pres1 {
+    public static void main(String[] args) {
+        DaoImplV2 dao = new DaoImplV2();
+        MetierImpl metier = new MetierImpl(dao);
+        //metier.setiDao(dao);
+
+        System.out.println( metier.calcul());
+    }
 }
